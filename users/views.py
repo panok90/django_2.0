23a8f9af251cb2e4746807dba5a6f0774a0dea2c
panok90 <cool.cyclon@yandex.ru>
2021-09-17 12:30:06 +0300

    created context processor

--- users/views.py
+++ users/views.py
@@ -76,10 +76,11 @@ class UserProfileView(CommonContextMixin, UpdateView):
     def get_success_url(self):
         return reverse_lazy('users:profile', args=(self.object.id,))
 
-    def get_context_data(self, **kwargs):
-        context = super(UserProfileView, self).get_context_data(**kwargs)
-        context['baskets'] = Basket.objects.filter(user=self.object)
-        return context
+# реализовано через контексный процессор
+#    def get_context_data(self, **kwargs):
+#        context = super(UserProfileView, self).get_context_data(**kwargs)
+#        context['baskets'] = Basket.objects.filter(user=self.object)
+#        return context
 
 
 class UserLogoutView(LogoutView):
