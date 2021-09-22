04768b8af9765182587913a75adf1c822a5f665b
panok90 <cool.cyclon@yandex.ru>
2021-09-22 12:50:03 +0300

    finished lesson2

--- users/models.py
+++ users/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.timezone import now
 from datetime import timedelta
 
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True)
-
+    age = models.PositiveIntegerField(verbose_name='Возраст', null=True)
     activation_key = models.CharField(max_length=128, blank=True)
     activation_key_expires = models.DateTimeField(default=(now() + timedelta(hours=48)))
 
@@ -15,3 +17,44 @@ class User(AbstractUser):
             return False
         else:
             return True
+
+
+class ShopUserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'W'
+    GENDER_CHOISES = (
+        (MALE, 'М'),
+        (FEMALE, 'Ж'),
+    )
+    user = models.OneToOneField(
+        User,
+        unique=True,
+        null=False,
+        db_index=True,
+        on_delete=models.CASCADE
+    )
+    tagline = models.CharField(
+        verbose_name='тэги',
+        max_length=128,
+        blank=True,
+    )
+    aboutme = models.TextField(
+        verbose_name='о себе',
+        max_length=512,
+        blank=True,
+    )
+    gender = models.CharField(
+        verbose_name='пол',
+        max_length=1,
+        choices=GENDER_CHOISES,
+        blank=True,
+    )
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.shopuserprofile.save()
