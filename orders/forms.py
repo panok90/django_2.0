bf099163abf15745e6db065fbbcd1d61fd7abd51
panok90 <cool.cyclon@yandex.ru>
2021-10-05 22:05:16 +0300

    finished lesson 5

--- orders/forms.py
+++ orders/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+
+from products.models import Product
 from .models import Order, OrderItem
 
 
@@ -24,3 +26,4 @@ class OrderItemForm(forms.ModelForm):
         super(OrderItemForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
+        self.fields['product'].queryset = Product.get_items()
--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
@@ -16,6 +17,11 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.name} | {self.category.name}'
+
+    @staticmethod
+    def get_items():
+        return Product.objects.filter(is_active=True).order_by('category', 'name')
